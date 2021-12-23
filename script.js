6035969d6563cd7864bc3248373aea65c1802268
ShaneC2021 <84458099+ShaneC2021@users.noreply.github.com>
2021-12-23 14:39:08 -0330

    successfully gathers data from rest API if local storage is empty, and prints to table, if local storage is not empty, local storage is printed to the screen

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let form = document.getElementById("myForm");
 
 // prints data in local storage to UI on page load
-document.addEventListener("DOMContentLoaded",  () => {
-  let retrievedArray = JSON.parse(localStorage.getItem("array"));
-  if (retrievedArray !== null) {
-    loadTableData(retrievedArray);
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  const fetchTodos = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then(function (response) {
+        // handle success
+        addToListFromApi(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+  };
+
+  fetchTodos();
 });
 
 form.addEventListener("submit", function (event) {
@@ -29,7 +39,7 @@ function addToList() {
 
   let toDoItem = {
     index: "",
-    item: document.getElementById("item").value,
+    title: document.getElementById("item").value,
   };
 
   // however if storage is empty arrayOfTodoList stores new data, upates index, then updates localStorage
@@ -41,6 +51,24 @@ function addToList() {
   loadTableData(arrayOfTodoList);
 }
 
+function addToListFromApi(responseObject) {
+  let arrayOfTodoList = [];
+  let retrievedArray = JSON.parse(localStorage.getItem("array")); // gets data stored under the key "array"
+
+  if (retrievedArray !== null) {
+    arrayOfTodoList = retrievedArray; // if storage is not empty this variable gets what is in local Storage which would be an array of objects
+    loadTableData(arrayOfTodoList);
+  } else if (retrievedArray === null) {
+    for (let i = 0; i < responseObject.data.length; i++) { // loads array with data from response object received from rest API
+      responseObject.data[i].index = i;                   // gives data an index property
+      arrayOfTodoList.push(responseObject.data[i]);
+    }
+  }
+  localStorage.setItem("array", JSON.stringify(arrayOfTodoList));
+  loadTableData(arrayOfTodoList);
+}
+
+
 function loadTableData(array) {
   let tableBody = document.getElementById("tableData");
   let row = "";
@@ -50,7 +78,7 @@ function loadTableData(array) {
     let rank = i + 1;
     row = `<tr>
                <td>${rank}</td>
-               <td>${array[i].item}</td>
+               <td>${array[i].title}</td>
                <td><i class="bi bi-trash m-1 garbage" onclick="trash(${array[i].index})"></i></td>
            </tr>`;
     tableBody.innerHTML += row;
@@ -86,4 +114,12 @@ function updateIndex(array) {
   for (let i = 0; i < array.length; i++) {
     array[i].index = i;
   }
+}
+
+// clears localstorage and clears table
+function clearStorage() {
+  let tableBody = document.getElementById("tableData");
+
+  localStorage.clear();
+  tableBody.innerHTML = "";
 }
